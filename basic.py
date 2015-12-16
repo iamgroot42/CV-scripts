130d0eff35e0bc1b96dfb6bf3c8dcc34d447ae5a
Anshuman <anshuman14021@iiitd.ac.in>
2015-12-16 22:29:25 +0530

    Combining two images

--- basic.py
+++ basic.py
@@ -2,7 +2,15 @@ import numpy as np
 import cv2
 
 # Load an color image in grayscale
-img = cv2.imread('house.tiff',0)
-cv2.imshow('House',img)
+img = cv2.imread('house.tiff',1)
+img2 = cv2.imread('jelly.tiff',1)
+b,g,r = cv2.split(img)
+# Or :
+# b=img[:,:,0]
+# g=img[:,:,1]
+# r=img[:,:,2]
+alpha = 0.29
+x = cv2.addWeighted(img,alpha,img2,1-alpha,0)
+cv2.imshow('Jelly+House',x)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
