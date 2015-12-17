b8185bd74928c3094462eb5eb3df7430db851e39
Anshuman <anshuman14021@iiitd.ac.in>
2015-12-17 22:53:53 +0530

    Added testing images:

--- basic.py
+++ basic.py
@@ -1,16 +1,36 @@
 import numpy as np
 import cv2
 
-# Load an color image in grayscale
-img = cv2.imread('house.tiff',1)
-img2 = cv2.imread('jelly.tiff',1)
-b,g,r = cv2.split(img)
-# Or :
+img = cv2.imread('coins.png',0)
+
+# b,g,r = cv2.split(img)
+# Or
 # b=img[:,:,0]
 # g=img[:,:,1]
 # r=img[:,:,2]
-alpha = 0.29
-x = cv2.addWeighted(img,alpha,img2,1-alpha,0)
-cv2.imshow('Jelly+House',x)
+
+# alpha = 0.29
+# x = cv2.addWeighted(img,alpha,img2,1-alpha,0)
+
+# Swapping R/B channels:
+# img[:,:,2],img[:,:,0]=img[:,:,0],img[:,:,2]
+# img-=img.mean()
+# img=(img)/(2*img.std())
+
+# Blur
+# img = cv2.blur(img,(15,15))
+
+# Median Blur
+# img = cv2.medianBlur(img,5)
+
+# Bilateral Filter
+# img = cv2.bilateralFilter(img,9,75,75)
+
+# Gaussian Blur 
+img = cv2.GaussianBlur(img,(7,7),0)
+
+# Canny:
+img = cv2.Canny(img,img.min(),img.max())
+
+cv2.imshow('Modern Art',img)
 cv2.waitKey(0)
-# cv2.destroyAllWindows()
