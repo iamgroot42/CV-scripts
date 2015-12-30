b6037fe2d6dbe5b3324ded628725ce49d8e64d97
Anshuman <anshuman14021@iiitd.ac.in>
2015-12-30 10:01:43 +0530

    Cleaned.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Homography and Affine Estimator 
 
-### An estimator which takes two images and estimates the homography/affine transformation between them.
+An estimator which takes two images and estimates the homography/affine transformation between them.
 
--- sift.py
+++ sift.py
@@ -1,16 +1,32 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('a.jpg')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
- 
-sift = cv2.xfeatures2d.SIFT_create()
-kp = sift.detect(gray,None)
- 
-img=cv2.drawKeypoints(gray,kp)
-cv2.imwrite('sift_keypoints.jpg',img)
-cv2.waitKey(0)
+# Harris corner detection 
+filename = 'a.jpg'
+filename2 = 'b.jpg'
+img = cv2.imread(filename)
+img2 = cv2.imread(filename2)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
-# r = 800.0 / img.shape[1]
-# dim = (800, int(img.shape[0] * r))
-# resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+gray = np.float32(gray)
+gray2 = np.float32(gray2)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+dst2 = cv2.cornerHarris(gray2,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+dst2 = cv2.dilate(dst2,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+img2[dst2>0.01*dst2.max()]=[0,0,255]
+
+r = 600.0 / img.shape[1]
+dim = (600, int(img.shape[0] * r))
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+resized2 = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
+
+cv2.imshow('A',resized)
+cv2.imshow('B',resized2)
+cv2.waitKey(0)
