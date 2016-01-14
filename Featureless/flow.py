e5194992c6da8cecad94ce538e219d0909894a68
Anshuman <anshuman14021@iiitd.ac.in>
2016-01-14 11:44:57 +0530

    RGB image for flow estimation.

--- Featureless/flow.py
+++ Featureless/flow.py
@@ -22,6 +22,7 @@ hsv = np.zeros_like(img)
 hsv[...,1] = 255
 
 mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+
 hsv[...,0] = ang*180/np.pi/2
 hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
 bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
--- Featureless/self_flow.py
+++ Featureless/self_flow.py
@@ -6,8 +6,11 @@ if len(sys.argv) < 3:
 	print "Format : python self_flow.py <image1> <image2> "
 	exit()
 
-img = cv2.imread(sys.argv[1],0)
-img2 = cv2.imread(sys.argv[2],0)
+img_in = cv2.imread(sys.argv[1])
+img2_in = cv2.imread(sys.argv[2])
+
+img = cv2.cvtColor(img_in, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2_in, cv2.COLOR_BGR2GRAY)
 
 def window_flow(window,window2):
 	Ix = np.zeros(window.shape)
@@ -44,11 +47,6 @@ for i in range(r/5):
 	for j in range(c/5):
 		flow_x[i,j],flow_y[i,j] = window_flow(img[5*i:5*i+5,5*j:5*j+5],img2[5*i:5*i+5,5*j:5*j+5])
 
-print "Horizontal flow"
-print flow_x
-print "Vertical flow"
-print flow_y
-
 img_x = np.zeros(img.shape)
 img_y = np.zeros(img.shape)
 for i in range(r):
@@ -56,15 +54,12 @@ for i in range(r):
 		img_x[i,j] = flow_x[i/5,j/5]
 		img_y[i,j] = flow_y[i/5,j/5]				
 
-cv2.imshow('Horizontal gradient',img_x)
-cv2.imshow('Vertical gradient',img_y)
-cv2.waitKey(0)
+hsv = np.zeros_like(img_in)
+hsv[...,1] = 255
+mag, ang = cv2.cartToPolar(img_x, img_y)
 
-# hsv = np.zeros_like(img)
-# hsv[...,1] = 255
-# mag, ang = cv2.cartToPolar(img_x, img_y)
-# hsv[...,0] = ang*180/np.pi/2
-# hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-# rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-# cv2.imshow('flow',rgb)
-# cv2.waitKey(0)
+hsv[...,0] = ang*180/np.pi/2
+hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+cv2.imshow('Optical flow',rgb)
+cv2.waitKey(0)
