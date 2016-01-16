0da056cde3430c7d2ce497cc64466159bf1b6c2d
Anshuman <anshuman14021@iiitd.ac.in>
2016-01-16 11:34:33 +0530

    Iterative LK needs fixing (so does normal LK).

--- Featureless/lk_flow.py
+++ Featureless/lk_flow.py
@@ -1,13 +1,19 @@
+# Made by : Anshuman Suri
+
 import numpy as np
 import cv2
 import sys
 
-if len(sys.argv) < 3:
-	print "Format : python self_flow.py <image1> <image2> "
+if len(sys.argv) < 4:
+	print "Format : python self_flow.py <image1> <image2> window_size"
 	exit()
 
-img_in = cv2.imread(sys.argv[1])
-img2_in = cv2.imread(sys.argv[2])
+try:
+	img_in = cv2.imread(sys.argv[1])
+	img2_in = cv2.imread(sys.argv[2])
+except:
+	print "Error reading files"
+	exit()
 
 img = cv2.cvtColor(img_in, cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(img2_in, cv2.COLOR_BGR2GRAY)
@@ -26,46 +32,59 @@ def window_flow(window,window2):
 	Iy_It = np.sum(Iy*It)
 	mat1 = np.matrix([[Ix_Ix,Ix_Iy],[Ix_Iy,Iy_Iy]])
 	mat2 = np.matrix([[-Ix_It],[-Iy_It]])
-
 	try:
 		ans = np.linalg.inv(mat1) * mat2
 	except:
 		# Singular matrix : so almost no motion in this part
 		ans = [0,0]
 	return ans
+		# u is along X axis,v is along Y axis
 
 r,c = img.shape[:2]
-
 i=0
 j=0
+window_size = sys.argv[3]
 template = img2_in
 
 # Uncomment to calculate flow on whole image
-u,v = window_flow(img,img2)
-print u,v
+threshold = 10
+abs_error = np.sum((img-img2)**2)
+while abs_error > threshold:
+	u,v = window_flow(img,img2)
+	temp = np.zeros(img.shape)
+	temp[int(v):,int(u):] = img[:img.shape[0]-int(v),:img.shape[1]-int(u)]
+	# cv2.imshow('pot',temp)
+	# cv2.waitKey(0)
+	# x = input()
+	img = temp
+	abs_error = np.sum((img-img2)**2)
+	print abs_error," error"
+	print u,v
+
 point1 = (c/2 , r/2)
 point2 = (int(round(c/2+u)) , int(round(r/2+v)))
 cv2.arrowedLine(template,point1,point2,(255,0,0),3)
 cv2.imwrite('Optical Flow.jpg',template)
 exit()
 
-# flow_x = np.zeros((r/5+1,c/5+1))
-# flow_y = np.zeros((r/5+1,c/5+1))
+# Uncomment to calculate flow on windows
+# flow_x = np.zeros((r/window_size+1,c/window_size+1))
+# flow_y = np.zeros((r/window_size+1,c/window_size+1))
 
-# for i in range(r/5):
-# 	for j in range(c/5):
-# 		flow_x[i,j],flow_y[i,j] = window_flow(img[5*i:5*i+5,5*j:5*j+5],img2[5*i:5*i+5,5*j:5*j+5])
+# for i in range(r/window_size):
+# 	for j in range(c/window_size):
+# 		flow_x[i,j],flow_y[i,j] = window_flow(img[window_size*i:window_size*i+window_size,window_size*j:window_size*j+window_size],img2[window_size*i:window_size*i+window_size,window_size*j:window_size*j+window_size])
 
 # img_x = np.zeros(img.shape)
 # img_y = np.zeros(img.shape)
 # for i in range(r):
 # 	for j in range(c):
-# 		img_x[i,j] = flow_x[i/5,j/5]
-# 		img_y[i,j] = flow_y[i/5,j/5]			
+# 		img_x[i,j] = flow_x[i/window_size,j/window_size]
+# 		img_y[i,j] = flow_y[i/window_size,j/window_size]			
 
-# for i in range(r/5):
-# 	for j in range(c/5):
-# 		point1 = (c/2 , r/2)
-# 		point2 = (int(round(c/2+flow_x[i,j])) , int(round(r/2+flow_y[i,j])))
+# for i in range(r/window_size):
+# 	for j in range(c/window_size):
+# 		point1 = (j*window_size+(window_size/2) , i*window_size+(window_size/2))
+# 		point2 = (int(round(j*window_size+(window_size/2)+flow_x[i,j])) , int(round(i*window_size+(window_size/2)+flow_y[i,j])))
 # 		cv2.arrowedLine(template,point1,point2,(255,0,0))
 # cv2.imwrite('Optical Flow(5x5).jpg',template)
--- Homography/feature_mapping.py
+++ Homography/feature_mapping.py
@@ -1,3 +1,5 @@
+# Made by : Anshuman Suri
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
