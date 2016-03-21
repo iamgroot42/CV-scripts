2468702bf41268cfada6936dd25c871c5fdbe994
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-21 17:05:24 +0530

    Face tracking across multiple frames. Dumped points into JSON file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## openCV examples (Python)
 A set of functions (useful for CV) implemented using openCV for Python.
+
+## Note
+You may have to run the following command :
+`  sudo ldconfig /usr/local/cuda/lib64 `
+(`import cv2` gives errors in some systems)
--- Tracking/face_track.py
+++ Tracking/face_track.py
@@ -1,28 +1,44 @@
-import numpy as np
 import cv2
 import sys
+import json
+import os
 
-if len(sys.argv) < 4:
-	print "Format : python "+sys.argv[0]+"<input> <image/video>"
-	exit()
+frames = os.listdir("Images")
+frames.sort()
 
-if sys.argv[2].lower() not in ["image","video"]:
-	print "Choose from image/video"
-	exit()
+coor_dump = []
 
-if sys.argv[2].lower() is "image":
-	data = cv2.imread(sys.argv[1])
-	gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+try:
+	for frame in frames:
+		data = cv2.imread("Images/"+frame)
+		gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+			
+		face_cascade = cv2.CascadeClassifier('Features/haarcascade_frontalface_default.xml')
+		# eye_cascade = cv2.CascadeClassifier('Features/haarcascade_eye.xml')
+		
+		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+		frame_coor = []
+		for (x,y,w,h) in faces:
+		    data = cv2.rectangle(data,(x,y),(x+w,y+h),(255,0,0),2)
+		    roi_gray = gray[y:y+h, x:x+w]
+		    roi_color = data[y:y+h, x:x+w]
+		    # print "(",x,",",y,",",w,",",h,")"
+		    frame_coor.append({"x":x+(w/2),"y":y+(h/2)})
+	
+		coor_dump.append(frame_coor)
+		    # eyes = eye_cascade.detectMultiScale(roi_gray)
+		    # for (ex,ey,ew,eh) in eyes:
+		    #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		
+		# cv2.imshow('data',data)
+		# cv2.waitKey(0)
+		# cv2.destroyAllWindows()
+except:
+	print "I/O error"
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    data = cv2.rectangle(data,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = data[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('data',data)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+try:
+	with open('Graphs/points.json', 'w') as outfile:
+	    json.dump(coor_dump, outfile)
+	print "Dumped JSON"
+except:
+	print "I/O error"
