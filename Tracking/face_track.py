e3522e1ff6b7fa2ee0bc5820106f07236b7eee00
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-26 20:30:02 +0530

    Minor changes

--- Tracking/face_track.py
+++ Tracking/face_track.py
@@ -10,28 +10,22 @@ coor_dump = []
 
 try:
 	time = 1
+	face_cascade = cv2.CascadeClassifier('Features/haarcascade_frontalface_default.xml')
+	
 	for frame in frames:
 		data = cv2.imread("Images/"+frame)
-		gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-			
-		face_cascade = cv2.CascadeClassifier('Features/haarcascade_frontalface_default.xml')
-		# eye_cascade = cv2.CascadeClassifier('Features/haarcascade_eye.xml')
-		
+		gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)		
+
 		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 		frame_coor = []
 		for (x,y,w,h) in faces:
 		    data = cv2.rectangle(data,(x,y),(x+w,y+h),(255,0,0),2)
 		    roi_gray = gray[y:y+h, x:x+w]
 		    roi_color = data[y:y+h, x:x+w]
-		    # print "(",x,",",y,",",w,",",h,")"
 		    frame_coor.append({"x":x+(w/2),"y":y+(h/2)})
 			
 		coor_dump.append({"time":time,"data":frame_coor})
 		time += 1
-		    # eyes = eye_cascade.detectMultiScale(roi_gray)
-		    # for (ex,ey,ew,eh) in eyes:
-		    #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-		
 		# cv2.imshow('data',data)
 		# cv2.waitKey(0)
 		# cv2.destroyAllWindows()
