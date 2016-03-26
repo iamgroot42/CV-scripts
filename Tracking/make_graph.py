7c6d87a184fee42f24277b328c04e305b7af7f5e
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-26 23:30:10 +0530

    Made graph plotting function generic

--- Tracking/make_graph.py
+++ Tracking/make_graph.py
@@ -1,37 +1,44 @@
 import json
-from operator import itemgetter
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import rcParams
 import matplotlib.pyplot as plt
 import numpy as np
 import Tkinter
+from operator import itemgetter
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rcParams
 
+colors = ['DarkMagenta','Blue','Yellow']
 
 with open('Graphs/points.json') as data_file:    
     data = json.load(data_file)
 
-d = sorted(data, key=itemgetter('time'))
+# Arrange frames chronologically
+frames = sorted(data, key=itemgetter('time'))
 
-x1 = []
-y1 = []
-x2 = []
-y2 = []
-z = []
+nPeople = len(frames[0]['data'])
 time = 1
 
-for e in d:
-	faces = sorted(e['data'], key=itemgetter('y'))
-	y1.append(faces[0].get('y'))
-	y2.append(faces[1].get('y'))
-	x1.append(faces[0].get('x'))
-	x2.append(faces[1].get('x'))
-	z.append(time)
+X = [[] for x in range(nPeople)]
+Y = [[] for x in range(nPeople)]
+Z = []
+
+# Sort by y-coordinate to approximate which user is which in the next frame. Not the
+# best way to do it, but works for the given dataset
+for frame in frames:
+	faces = sorted(frame['data'], key=itemgetter('y'))
+	for i in range(nPeople):
+		X[i].append(faces[i].get('x'))
+		Y[i].append(faces[i].get('y'))
+	Z.append(time)
 	time += 1
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot(x1,y1,z,label = 'Tracking1', color = 'DarkMagenta', linewidth = 3.2,)
-ax.plot(x2,y2,z,label = 'Tracking2', color = 'Green', linewidth = 3.2,)
+
+# Plotting coordinates for every tracked person
+for i in range(nPeople):
+	ax.plot(X[i],Y[i],Z,label = str(i+1), color = colors[i], linewidth = 3.2)
+	print "alele"
+
 rcParams['legend.fontsize'] = 11
 ax.legend()
 
@@ -39,12 +46,10 @@ ax.set_xlabel('X axis')
 ax.set_xlim(0,1000)
 ax.set_ylabel('Y axis')
 ax.set_ylim(0,1000)
-ax.set_zlabel('Time')
+ax.set_zlabel('Time (s)')
 ax.set_zlim(0,40)
 
-ax.set_title('3D line plot, \n Face Tracking', va='bottom')
+ax.set_title('Face Tracking', va='bottom')
 ax.view_init(elev=18, azim=-27)
 ax.dist=9
 plt.show()
-
-	
