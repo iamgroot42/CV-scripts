0593e6f4fcb934e781fd6e4e389de84b2d5939ea
Mansi Goel <mansi14062@iiitd.ac.in>
2016-05-04 02:06:41 +0530

    statistical analysis done

---
+++ Tracking/Graphs/med_mean.py
@@ -0,0 +1,96 @@
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import Tkinter
+import sys
+from operator import itemgetter
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rcParams
+import math
+import csv
+from itertools import islice
+import sys
+import os
+
+rootdir = 'Outputs/'
+d = set()
+for subdir, dirs, files in os.walk(rootdir):
+	for file in files:
+		k = os.path.join(subdir, file)
+		c = open(k)
+		data = csv.reader(c)
+		datal = list(data)
+		row_count = len(datal)
+		# print row_count
+		count = 0;
+		c.seek(0)
+		numcol = 0
+		numrow = 0
+		for i in data:
+			if count == 2:
+				numcol = len(i)
+				numrow = (numcol-1)/2
+				rowtemp = numrow
+				# print numcol
+			count += 1
+		c.seek(0)
+		all = []
+		allm = []
+		row_flag = 3
+		tempc=[]
+		tempr = []
+		temprm = []
+		tempcm =[]
+		for x in range(0,numrow):
+			tempr = []
+			temprm = []
+			for y in range(1, numcol):
+				c.seek(0)
+				count = 0
+				tempc = []
+				tempcm = []
+				for i in data:
+					# print i
+					if (count-row_flag)%(rowtemp+3)==0:
+						# print i
+						tempc.append(i[y])
+						tempcm.append(i[y])
+					count = count+1
+				r = map(float, tempc)
+				rm = map(float, tempcm)
+				q = np.mean(r)
+				qm = np.median(rm)
+				tempr.append(q)
+				temprm.append(qm)
+			all.append(tempr)
+			allm.append(temprm)
+			row_flag += 1
+		alls = []
+		alls.append(['mean'])
+		for g in all:
+			alls.append(g)
+		alls.append([])
+		alls.append(['median'])
+		for g in allm:
+			alls.append(g)
+
+		c.close()
+		x1,x2 = os.path.split(k)
+		b = os.path.splitext(x2)[0]
+		b = 'Final/'+b+'med_mean.csv'
+		print b
+		cw = open(b,'w')
+		rw = csv.writer(cw, lineterminator='\n')
+		rw.writerows(alls)
+		cw.close()
+
+
+
+
+
+
+
+# She wanted only the sparkle of his eyes,
+# He promised her to bring the stars of skies,
+# With another promise she was again left out,
+# Words worked again, the evil inside him said aloud
---
+++ Tracking/Graphs/tables.py
@@ -0,0 +1,90 @@
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import Tkinter
+import sys
+from operator import itemgetter
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rcParams
+import math
+import csv
+from itertools import islice
+import sys
+import os
+
+rootdir = 'Values/'
+d = set()
+for subdir, dirs, files in os.walk(rootdir):
+	for file in files:
+		d.add(os.path.join(subdir))
+
+d = sorted(d)
+dl = list(d)
+for x in dl:
+	all = []
+	for subdir, dirs, files in os.walk(x):
+		files.sort()
+		for file in files:
+			try:
+				s = os.path.join(subdir,file)
+				print s
+				with open(s,'rw') as data_file:
+					k = []
+					k.append(file)
+					all.append(k)
+				   	data = json.load(data_file)
+			except:
+				print "I/O error"
+				exit()
+
+			keylist = data.keys()
+			keylist.sort()
+			top = []
+			first = []
+			top.append('-')
+			first.append('Object')
+			for p in keylist:
+				first.append('YY')
+				top.append(p)
+				# first.append('YX')
+				# top.append('-')
+				first.append('XX')
+				top.append('-')
+				# first.append('XY')
+				# top.append('-')
+			all.append(top)
+			all.append(first)
+
+			for y in keylist:
+				row = []
+				row.append(y)
+				val = data.get(y)
+				for z in val:
+					temp = []
+					yy = z.get('YY')
+					yx = z.get('YX')
+					xx = z.get('XX')
+					xy = z.get('XY')
+					temp.append(yy)
+					# temp.append(yx)
+					temp.append(xx)
+					# temp.append(xy)
+					row = row + temp
+				all.append(row)
+			# temp=[]
+			# all.append(temp)
+
+		x1,x2 = os.path.split(x)
+		b = x2+'_table.csv'
+		x3,x4 = os.path.split(x1)
+		b = 'Outputs/' +x4+b
+		cw = open(b, 'w')
+		rw = csv.writer(cw, lineterminator='\n')
+		rw.writerows(all)
+		cw.close()
+
+
+
+
+
+
